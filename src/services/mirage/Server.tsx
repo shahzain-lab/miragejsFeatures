7aee4c856aebab862ae2185840e25cc9f6563b47
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-08 02:17:58 -0800

    creating models server and errorHandels

--- src/services/mirage/Server.tsx
+++ src/services/mirage/Server.tsx
@@ -1,2 +1,25 @@
 import { Server, Model, Factory, belongsTo, hasMany, Response } from 'miragejs';
 
+
+///handleErrors response, if any problem while communicating with server
+export const handleErrors = (error: any, message = "An error ocurred") => {
+  return new Response(400, undefined, {
+      data: {
+          message,
+          isError: true,
+      },
+  })
+};
+
+//our server
+export const setUpServer = (env?: string): Server => {
+    return new Server({
+        environment: env ?? 'development',\
+        //creating models for dynamics data
+        models: {
+            entry: Model.extend({
+                diary: belongsTo(),
+            }),
+        }
+    })
+};
