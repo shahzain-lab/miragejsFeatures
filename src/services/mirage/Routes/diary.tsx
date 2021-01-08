7764ef05264ce39c087c08edd728e43456649293
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-08 03:27:00 -0800

    Update diary.tsx

--- src/services/mirage/Routes/diary.tsx
+++ src/services/mirage/Routes/diary.tsx
@@ -1,6 +1,7 @@
 import { User } from "../../../interfaces/User.interface";
 import { Diary } from "../../../interfaces/Diary.interface";
 import { handleErrors } from "../Server";
+import { Entry } from "../../../interfaces/Entry.interface";
 
 export const create = (
     schema: any,
@@ -30,4 +31,50 @@ export const create = (
     }
 }
 
-export const updateDiary = (schema: any, req: Request): Diary | Response
+export const updateDiary = (schema: any, req: Request): Diary | Response => {
+    try{
+        const diary = schema.diaries.find( req.params.id );
+        const data = JSON.parse(req.requestBody) as Partial<Diary>;
+        const now = dayjs().format();
+        diary.update({
+            ...data,
+            updatedAt: now,
+        });
+        return diary.attrs as Diary;
+    } catch (error){
+        return handleErrors(error, 'Failed to update diary.');
+    }
+};
+
+export const getDiaries = (schema: any, req: Request): Diary[] | Response => {
+    try{
+        const user = schema.users.find(req.params.id);
+        return user.diary as Diary[];
+    }catch (error){
+        return handleErrors(error, 'could not get user diaries')
+    }
+}
+////
+export const addEntry = (
+    schema: any,
+    req: Request
+): { diary:Diary; entry: Entry } | Response => {
+    try{
+        const diary = schema.diaries.find(req.params.id);
+        const {title, content} = JSON.parse(req.requestBody) as Partial<Entry>;
+        const now = dayjs().format();
+        const entry = diary.createEntry({
+            title,
+            content,
+            createdAt: now,
+            updatedAt: now,
+        });
+        diary.update({
+            ...diary.attrs,
+            updatedAt: now
+        });
+        return {
+            
+        }
+    }
+}
