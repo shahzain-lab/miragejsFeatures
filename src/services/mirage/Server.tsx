e6dcd481f58ff9b52f47197378870f113255e2c9
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-08 02:32:30 -0800

    adding routes, seeds and factories

--- src/services/mirage/Server.tsx
+++ src/services/mirage/Server.tsx
@@ -14,12 +14,38 @@ export const handleErrors = (error: any, message = "An error ocurred") => {
 //our server
 export const setUpServer = (env?: string): Server => {
     return new Server({
-        environment: env ?? 'development',\
+        environment: env ?? 'development',
         //creating models for dynamics data
         models: {
             entry: Model.extend({
                 diary: belongsTo(),
             }),
-        }
-    })
+            diary: Model.extend({
+                entry: hasMany(),
+                user:belongsTo()
+            }),
+            user: Model.extend({
+                diary:hasMany()
+            }),
+        },
+
+        ///Factories are the perfect place to encode the constraints of your seeding data
+        factories: {
+            user: Factory.extend({
+                username: 'test',
+                password: 'password',
+                email: "Shahzain@gmail.com",
+            }),
+        },
+           
+        //seed are used for initial data that we have above 
+        seeds: (server): any => {
+            server.create('user');
+        },
+
+        ///routes for communicating with network requests
+        routes():void {
+            this.urlPrefix = 'https://diaries.app';
+        },
+    });
 };
